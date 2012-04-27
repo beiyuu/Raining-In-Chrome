f784b84d821894d9f815d8105c294b35bfa01db6
BeiYuu <dewreg@gmail.com>
2012-04-27 21:19:00 +0800

    thr plugin source

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Raining-In-Chrome
 =================
 
 Raining makes everything better.
+This is a chrome plugin makes you can listen to the raining in your Chrome browser.
---
+++ background.js
@@ -0,0 +1,23 @@
+﻿var hasSetRain = false,isPlay = false,player = document.getElementById('player');
+
+function togglePlay(){
+    if(isPlay){
+        isPlay = false;
+        player.pause();
+        chrome.browserAction.setIcon({path: 'assets/sun.png'});
+        chrome.browserAction.setTitle({title:'Raining makes everything better!'});
+    }else{
+        isPlay = true;
+        player.play();
+        chrome.browserAction.setIcon({path: 'assets/rain.png'});
+        chrome.browserAction.setTitle({title:"Listen it's raining!"});
+    }
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    if(!hasSetRain){
+        player.setAttribute('src','http://174.36.223.28/audio/RainyMood.m4a');
+        hasSetRain = true;
+    }
+    togglePlay();
+});
